5c8cd06cd7b5c6168fe818011aaf8df8b819d80e
Paulo Chaves <opaulochaves@gmail.com>
2025-07-08 11:06:53 -0300

    add java pack

--- lua/community.lua
+++ lua/community.lua
@@ -13,5 +13,22 @@ return {
   { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.java" },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          configuration = {},
+        },
+        format = {
+          enabled = true,
+          settings = { -- you can use your preferred format style
+            url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+            profile = "GoogleStyle",
+          },
+        },
+      },
+    },
+  },
 }
