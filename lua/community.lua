64b712a411d5f2ef7879474c03da327520412883
Paulo Chaves <opaulochaves@gmail.com>
2025-10-07 14:19:17 -0300

    update

--- lua/community.lua
+++ lua/community.lua
@@ -13,22 +13,58 @@ return {
   { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.java" },
-  {
-    "mfussenegger/nvim-jdtls",
-    opts = {
-      settings = {
-        java = {
-          configuration = {},
-        },
-        format = {
-          enabled = true,
-          settings = { -- you can use your preferred format style
-            url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
-            profile = "GoogleStyle",
-          },
-        },
-      },
-    },
-  },
+  -- { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.java" },
+  -- {
+  --   "mfussenegger/nvim-jdtls",
+  --   opts = {
+  --     settings = {
+  --       java = {
+  --         configuration = {
+  --           runtimes = {
+  --             {
+  --               name = "JavaSE-25",
+  --               path = "/Users/opchav/.local/share/mise/installs/java/openjdk-25/",
+  --             },
+  --           },
+  --         },
+  --       },
+  --       format = {
+  --         enabled = true,
+  --         settings = { -- you can use your preferred format style
+  --           url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+  --           profile = "GoogleStyle",
+  --         },
+  --       },
+  --     },
+  --     cmd = (function()
+  --       vim.env.JAVA_HOME = vim.fn.expand "~/.local/share/mise/installs/java/openjdk-25/"
+  --
+  --       local jdtls_bin = vim.fn.stdpath "data" .. "/mason/bin/jdtls"
+  --       local jdtls_pkg = vim.fn.stdpath "data" .. "/mason/packages/jdtls"
+  --       local workspace = vim.fn.stdpath "cache" .. "/nvim/jdtls/workspace"
+  --
+  --       local cmd = {
+  --         jdtls_bin,
+  --         "--jvm-arg=-javaagent:" .. jdtls_pkg .. "/lombok.jar",
+  --         "-configuration",
+  --         jdtls_pkg .. "/config_mac",
+  --         "-data",
+  --         workspace,
+  --       }
+  --
+  --       -- Notify the full command for debugging
+  --       vim.schedule(
+  --         function()
+  --           vim.notify(
+  --             "Starting jdtls with command:\n" .. table.concat(cmd, " "),
+  --             vim.log.levels.INFO,
+  --             { title = "jdtls cmd" }
+  --           )
+  --         end
+  --       )
+  --     end)(),
+  --   },
+  -- },
 }
