ed1315ce5fab10ba88fad8f3ba9300bb2c865b52
Paulo Chaves <opaulochaves@gmail.com>
2026-05-09 11:18:49 -0300

    add nvim-lint

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,20 @@
+---@type LazySpec
+return {
+  "mfussenegger/nvim-lint",
+  event = "User AstroFile",
+  config = function()
+    local lint = require "lint"
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      vue = { "eslint_d" },
+      svelte = { "eslint_d" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+      callback = function() lint.try_lint() end,
+    })
+  end,
+}
